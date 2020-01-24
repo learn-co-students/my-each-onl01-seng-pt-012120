ed64127e55ca4a99993370202b0c3644243af9e0
Karen Daniela <kdaniimora@gmail.com>
2020-01-24 19:27:54 +0000

    Done.

--- my_each.rb
+++ my_each.rb
@@ -1,9 +1,8 @@
-def my_each[1, 2, 3, 4]
-  
- my_each(array) do |i|
-         
-         counter+=1
-       end
-
- end
- 
+def my_each(collection)
+  i = 0
+  while i < collection.length
+    yield(collection[i])
+    i = i + 1
+  end
+  collection
+end
