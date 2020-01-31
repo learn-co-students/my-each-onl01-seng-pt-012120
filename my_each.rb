49280126e75a607b301fd197aaa3e3cb28d3f419
Hailey P <heyputnam@gmail.com>
2020-01-31 16:44:41 +0000

    Done.

--- my_each.rb
+++ my_each.rb
@@ -1,3 +1,15 @@
-def my_each # put argument(s) here
-  # code here
+words = ['hi', 'hello', 'bye', 'goodbye']
+tas = ['arel', 'jon', 'logan', 'spencer']
+
+def my_each(words)
+i = 0
+while i < words.length
+yield words[i]
+i = i + 1
+end
+return words
+end
+
+my_each(words) do |word|
+print word
 end
