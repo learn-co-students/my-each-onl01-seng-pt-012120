b0b8eb90db804ec3a76754e883d7492e8a25d27a
Patricia C <patricia@patriciaco.com>
2020-02-03 01:43:50 +0000

    Done.

--- my_each.rb
+++ my_each.rb
@@ -1,3 +1,12 @@
-def my_each # put argument(s) here
-  # code here
+# def my_each # put argument(s) here
+#   # code here
+# end
+
+def my_each(day)
+  count = 0
+  while count < day.length
+  yield day[count]
+  count +=1
+end
+return day
 end
