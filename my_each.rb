3ca7c6797e7a7272c2c36f32961eba5ba7737920
Chris Beyer <beyercorpuz@gmail.com>
2020-02-03 20:39:15 +0000

    Done.

--- my_each.rb
+++ my_each.rb
@@ -1,3 +1,8 @@
-def my_each # put argument(s) here
-  # code here
+def my_each(items)
+  arr = items.collect{|item| item}
+  while arr.empty? == false do
+	  item = arr.shift
+		yield item
+	end
+	return items
 end
